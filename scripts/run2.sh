e1f3cfd7060f8feaf5e1f824186909781d41773e
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-07-02 20:00:36 +0200

    rename run2 to run and run3 to run2

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
-~/glslang/bin/glslangValidator -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 -DWIDTH=$2 -DHEIGHT=$3 --target-env vulkan1.2 -V $1 -o shaders/comp.spv
+echo "x:int,y:int,z:int,time_ms:int,threads:int,invocations:int,simd:int,thread_occupancy_pct:int" | tee runtime.csv
 
-rm -f mandelbrot.png
-ANV_ENABLE_PIPELINE_CACHE=0 MESA_GLSL_CACHE_DISABLE=1 GROUP_X=$4 GROUP_Y=$5 CSV=1 mygl.sh $GDB ./vulkan_minimal_compute
-if [ ! -f mandelbrot.png ]; then
-	echo "output file doesn't exist"
-	exit 1
-fi
+for x in 1 2 4 8 16 32 64 128 256 512; do
+	for y in 1 2 4 8 16 32 64 128 256 512; do
+		sz=$(($x * $y))
+		if [ $sz -le 1792 ]; then
+			./run2.sh $1 $2 $3 $x $y | grep -v -e "$1" -e "x:int" | tee -a runtime.csv
+			cp data.csv data_${2}x${3}_${x}x${y}.csv
+		fi
+	done
+done

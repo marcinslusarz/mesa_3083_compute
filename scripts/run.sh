cf6e36ae7ac817b0886803dde5f9a53e99bc808b
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-07-02 19:47:27 +0200

    disable shader cache

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 rm -f mandelbrot.png
-~/glsllang/bin/glslangValidator -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 -DWIDTH=$2 -DHEIGHT=$3 -target-env vulkan1.2 -V $1 -o shaders/comp.spv && make && $GDB mygl.sh ./vulkan_minimal_compute && xdg-open mandelbrot.png
+~/glsllang/bin/glslangValidator -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 -DWIDTH=$2 -DHEIGHT=$3 -target-env vulkan1.2 -V $1 -o shaders/comp.spv
+make
+ANV_ENABLE_PIPELINE_CACHE=0 MESA_GLSL_CACHE_DISABLE=1 $GDB mygl.sh ./vulkan_minimal_compute && xdg-open mandelbrot.png
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -3,7 +3,7 @@
 ~/glslang/bin/glslangValidator -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 -DWIDTH=$2 -DHEIGHT=$3 --target-env vulkan1.2 -V $1 -o shaders/comp.spv
 
 rm -f mandelbrot.png
-GROUP_X=$1 GROUP_Y=$2 CSV=1 mygl.sh $GDB ./vulkan_minimal_compute
+ANV_ENABLE_PIPELINE_CACHE=0 MESA_GLSL_CACHE_DISABLE=1 GROUP_X=$4 GROUP_Y=$5 CSV=1 mygl.sh $GDB ./vulkan_minimal_compute
 if [ ! -f mandelbrot.png ]; then
 	echo "output file doesn't exist"
 	exit 1

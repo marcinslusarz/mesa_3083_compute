e5217d451d0543cb5433748910b527554ae7ef6a
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-07-03 15:04:35 +0200

    use --quiet option for glslang

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-~/glslang/bin/glslangValidator -DWIDTH=$2 -DHEIGHT=$3 -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 --target-env vulkan1.2 -V $1 -o shaders/comp.spv
+~/glslang/bin/glslangValidator -DWIDTH=$2 -DHEIGHT=$3 -DWORKGROUP_SIZE_X=$4 -DWORKGROUP_SIZE_Y=$5 --target-env vulkan1.2 -V $1 -o shaders/comp.spv --quiet
 
 rm -f mandelbrot.png
 ANV_ENABLE_PIPELINE_CACHE=0 MESA_GLSL_CACHE_DISABLE=1 CSV=1 mygl.sh $GDB ./vulkan_minimal_compute $2 $3 $4 $5

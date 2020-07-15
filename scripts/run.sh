18d3733a0096d63cedc137db3e5b970786232fdf
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-07-15 15:19:39 +0200

    make subgroups optional

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-~/glslang/bin/glslangValidator -DWIDTH=$2 -DHEIGHT=$3 -DDEPTH=$4 -DWORKGROUP_SIZE_X=$5 -DWORKGROUP_SIZE_Y=$6 -DWORKGROUP_SIZE_Z=$7 --target-env vulkan1.2 -V $1 -o shaders/comp.spv --quiet
+~/glslang/bin/glslangValidator -DUSE_SUBGROUPS=1 -DWIDTH=$2 -DHEIGHT=$3 -DDEPTH=$4 -DWORKGROUP_SIZE_X=$5 -DWORKGROUP_SIZE_Y=$6 -DWORKGROUP_SIZE_Z=$7 --target-env vulkan1.2 -V $1 -o shaders/comp.spv --quiet
 
 rm -f result.png
 ANV_ENABLE_PIPELINE_CACHE=0 MESA_GLSL_CACHE_DISABLE=1 CSV=1 mygl.sh $GDB ./vulkan_minimal_compute $2 $3 $4 $5 $6 $7

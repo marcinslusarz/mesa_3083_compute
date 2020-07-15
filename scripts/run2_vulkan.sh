af8e8e5f7a50df4758ad72fd794f2f10651f1510
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-07-15 18:19:28 +0200

    rename scripts

--- scripts/run2_vulkan.sh
+++ scripts/run2_vulkan.sh
@@ -7,7 +7,7 @@ for x in 1 2 4 8 16 32 64 128 256 512; do
 		for z in 1 2 4 8 16 32 64; do
 			sz=$(($x * $y * $z))
 			if [ $sz -le 1792 ]; then
-				./run.sh $1 $2 $3 $4 $x $y $z
+				./run_vulkan.sh $1 $2 $3 $4 $x $y $z
 				cat stats.csv | csv-header -m | tee -a runtime.csv
 				cp data.csv data_${2}x${3}x${4}_${x}x${y}x${z}.csv
 			fi
